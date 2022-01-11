abda936645ae3760e58ce298c2db6276ddd06391
Umesh8155 <umesh.developer20@gmail.com>
2022-01-11 13:17:21 +0530

    add chart

---
+++ lib/ui/chart_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChartScreen extends StatefulWidget {
+  const ChartScreen({Key key}) : super(key: key);
+
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
